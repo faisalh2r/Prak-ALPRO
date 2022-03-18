33e1f24a60d87effb7b9d7eeeb2345f81ec92394
FDLNNN <101852832+FDLNNN@users.noreply.github.com>
2022-03-19 06:33:00 +0700

    Add files via upload

---
+++ kalkulator pembagian.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+
+    cout << "Inputkan nilai a: ";
+    cin >> a;
+    cout << "Inputkan nilai b: ";
+    cin >> b;
+
+    
+    c = a/b;
+
+    cout << "Hasil a / b = " << c << endl;
+
+    return 0;
+}
+
